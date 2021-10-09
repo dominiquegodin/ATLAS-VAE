cf4920572b5c83fe983e0595e75ee2ea5ad3d4be
Dominique Godin <dominique.godin@umontreal.ca>
2021-10-09 14:12:36 -0400

    Improved conversion script

--- tools/root2h5.sh
+++ tools/root2h5.sh
@@ -1,12 +1,31 @@
 source setup.sh
 
-# COMMAND LINES FOR SIGNAL SAMPLES: sbatch -w atlas16 sbatch.sh
-python root2h5.py --sample_type=topo-ttbar
-python root2h5.py --sample_type=UFO-ttbar
-python root2h5.py --sample_type=BSM
 
-# COMMAND LINES FOR BACKGROUND SAMPLES: sbatch -w atlas16 --array=0-9%1 sbatch.sh
-#if [ $SBATCH_VAR -le 9 ]; then python root2h5.py --sample_type=topo-dijet --tag $SBATCH_VAR; fi
-#if [ $SBATCH_VAR -le 9 ]; then python root2h5.py --sample_type=UFO-dijet --tag $SBATCH_VAR; fi
-#if [ $SBATCH_VAR -ge 9 ]; then python root2h5.py --merging=ON; fi
+# SIGNAL SAMPLES
+# COMMAND LINE: sbatch -w atlas16 sbatch.sh
+#python root2h5.py --sample_type=topo-ttbar
+#python root2h5.py --sample_type=UFO-ttbar
+#python root2h5.py --sample_type=BSM
+
+
+# BACKGROUND SAMPLES
+# COMMAND LINE: sbatch -w atlas16 --array=0-9%1 sbatch.sh
+sample_type=UFO-dijet #topo-dijet
+
+if [ $sample_type == UFO-dijet ] && ([ $SBATCH_VAR -eq 1 ] || [ $SBATCH_VAR -eq 2 ])
+then
+    library=ak
+else
+    library=np
+fi
+
+if [ $SBATCH_VAR -le 9 ]
+then
+    python root2h5.py --sample_type=$sample_type --library=$library --tag $SBATCH_VAR
+fi
+
+if [ $SBATCH_VAR -ge 9 ]
+then
+    python root2h5.py --merging=ON
+fi
 
